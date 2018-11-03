7fd3ae2fe3e342bbf5ac0cf834f2cc7a0bdc078d
Geert Huls <geert.huls82@gmail.com>
2018-11-03 22:01:54 +0100

    Number of cart items indicator

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,9 @@
                          class="nav-link" to="/cart" exact>
               Cart
             </router-link>
+            <div class="cart-items">
+              {{cart.length}}
+            </div>
           </li>
         </ul>
       </nav>
@@ -41,6 +44,11 @@
 
 export default {
   name: 'app',
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
 };
 </script>
 
@@ -111,5 +119,15 @@ ul {
   width: 100px;
   min-height: 300px;
 }
-
+.cart-items {
+  position: absolute;
+  top: -5px;
+  right: -9px;
+  font-size: 18px;
+  width: 20px;
+  text-align: center;
+  display: inline-block;
+  border-radius: 100px;
+  background-color: mediumseagreen;
+}
 </style>
