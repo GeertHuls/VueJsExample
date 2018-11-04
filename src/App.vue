d9f26f573d9280d8e3d2550e97aee7e2842c7d4a
Geert Huls <geert.huls82@gmail.com>
2018-11-04 22:30:17 +0100

    Tryout with state and getters in namespaced modules

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    Root Foo: {{rootFoo}} <br/>
+    Robots Foo: {{robotsFoo}} <br/>
+    Users Foo: {{usersFoo}} <br/>
+    <br/>
+    Root Getter Foo: {{rootGetterFoo}} <br/>
+    Robots Getter Foo: {{robotsGetterFoo}} <br/>
+    Users Getter Foo: {{usersGetterFoo}} <br/>
     <header>
       <nav>
         <ul>
@@ -45,6 +52,24 @@
 export default {
   name: 'app',
   computed: {
+    rootFoo() {
+      return this.$store.state.foo;
+    },
+    robotsFoo() {
+      return this.$store.state.robots.foo;
+    },
+    usersFoo() {
+      return this.$store.state.users.foo;
+    },
+    rootGetterFoo() {
+      return this.$store.getters.foo;
+    },
+    robotsGetterFoo() {
+      return this.$store.getters['robots/foo'];
+    },
+    usersGetterFoo() { // this will cause an error because the foo state also exists on the root
+      return this.$store.getters['users/foo'];
+    },
     cart() {
       return this.$store.state.robots.cart;
     },
